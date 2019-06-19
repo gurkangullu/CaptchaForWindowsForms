8a1b7738c210ef579e2d05a52a677b42ffd2c72f
gurkangullu <51590667+gurkangullu@users.noreply.github.com>
2019-06-20 00:55:34 +0300

    Code cleaned.

--- CaptchaForWinForms/WinFormCaptcha.cs
+++ CaptchaForWinForms/WinFormCaptcha.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-//Added Library:
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Text;
 using System.Drawing.Imaging;
+using System.Drawing.Text;
 using System.IO;
+using System.Linq;
 
 namespace CaptchaForWinForms
 {
